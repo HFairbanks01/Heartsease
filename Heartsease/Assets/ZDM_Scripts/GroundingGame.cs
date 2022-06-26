446c14451f7972a7ed3774deaa419baf4525f215
nnoejovich <nnoejovich@gmail.com>
2022-06-25 20:30:37 -0400

    Added Chests

--- Heartsease/Assets/ZDM_Scripts/GroundingGame.cs
+++ Heartsease/Assets/ZDM_Scripts/GroundingGame.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GroundingGame : MonoBehaviour
 {
+
+    public float playerPosition;
+    public GameObject box;
+    public GameObject square;
+    //public Transform spawnPoint;
+    public int TickTime = 1;
+    private float currentTime = 0;
+    private float randomNumber;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,21 @@ public class GroundingGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        currentTime += Time.deltaTime;
         
+        if (currentTime >= 1)
+        {
+            randomNumber = Random.Range(0, 1);
+            if (randomNumber == 0)
+            {
+                
+            }
+            currentTime = 0;
+        }
+    }
+
+    public void SpawnBox()
+    {
+        //var newBox = Instantiate(square, spawnPoint.position, spawnPoint.rotation);
     }
 }
