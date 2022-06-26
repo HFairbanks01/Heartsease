6900b376c9cf1d110cb6c9b67e2a36967a56bf07
HFairbanks01 <60548521+HFairbanks01@users.noreply.github.com>
2022-06-26 17:18:54 -0400

    final push

--- Heartsease/Assets/ZDM_Scripts/PetController.cs
+++ Heartsease/Assets/ZDM_Scripts/PetController.cs
@@ -6,11 +6,25 @@ public class PetController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public PlayerController player;
+    public bool hasHeart;
 
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, player.lastPoint.position, moveSpeed * Time.deltaTime);
+    }
+
+    public void Interact(PlayerController player)
+    {
+        if(player.playerHearts >= 1)
+        {
+            hasHeart = true;
+            player.ChangeHearts(-1);
+            player.ChangeHeartease(10f);
+        }
+        else{
+
+        }
     }
 }
